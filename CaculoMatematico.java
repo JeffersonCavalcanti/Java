fa4d38382f8af605d3139d10a517fd775698ba81
Jefferson Cavalcanti <83166260+JeffersonCavalcanti@users.noreply.github.com>
2022-12-30 18:01:57 -0300

    CalculoMatematico

---
+++ CaculoMatematico.java
@@ -0,0 +1,11 @@
+package CriandoTestePosGraduacao;
+
+public class CaculoMatematico{
+		public static float executaCauculoSomar (float V1,float V2) {
+		float somar = V1 + V2;
+		return somar;
+		}
+
+	
+
+}
