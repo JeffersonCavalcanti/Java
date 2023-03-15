3027a70c5ed8e600d0ef51cce00d72215ed3fb79
Jefferson Cavalcanti <83166260+JeffersonCavalcanti@users.noreply.github.com>
2023-03-14 23:52:09 -0300

    Update TesteWhile.java

--- TesteWhile.java
+++ TesteWhile.java
@@ -1,3 +1,20 @@
+name: Java CI
+
+on: [push]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v3
+      - name: Set up JDK 17
+        uses: actions/setup-java@v3
+        with:
+          java-version: '17'
+          distribution: 'temurin'
+      - name: Build with Ant
+        run: ant -noinput -buildfile build.xml
 package sintaxe_variavel_e_fluxo;
 
 public class TesteWhile {
